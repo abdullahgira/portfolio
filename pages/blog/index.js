3ea8ae878dfde5c0edc56af9bd841f912d7695ab
Xayden Rosario <xaydenrosario@gmail.com>
2020-12-17 22:25:25 +0200

    Create the blog home page style

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,25 @@
+import DateFormatter from "components/date-formatter"
+
 export default function Blog() {
-  return <h1>Blog</h1>
+  return (
+    <main className="container my-10 mx-auto">
+      <section className="flex items-center">
+        <div className="h-16 w-16 bg-green-400 rounded-full mr-5"></div>
+        <div>
+          <h3 className="text-md">
+            Personal blog by <strong>Abdullah Osama</strong>
+          </h3>
+          <small>I'm hooked to react.</small>
+        </div>
+      </section>
+      <section className="mt-10">
+        <article className="rounded-md px-7 p-5 bg-white shadow-md">
+          <h4 className="text-2xl font-bold mb-2">Post title</h4>
+          <p className="text-gray-500 text-xs">
+            <DateFormatter dateString={new Date().toISOString()} />
+          </p>
+        </article>
+      </section>
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '568px',
+      // lg: '1024px',
+      // xl: '1145px',
+    },
     extend: {},
   },
   variants: {
