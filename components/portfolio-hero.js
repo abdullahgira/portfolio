fad186cc042f0a42b008c41c85992a59086213d8
Xayden Rosario <xaydenrosario@gmail.com>
2020-12-18 20:58:55 +0200

    Hero buttons are now working

--- components/portfolio-hero.js
+++ components/portfolio-hero.js
@@ -1,4 +1,9 @@
+import React from "react"
+import Link from "next/link"
+
 export default function PortfolioHero() {
+  const [showEmail, setShowEmail] = React.useState(false)
+
   return (
     <div className="flex flex-col items-center md:flex-row md:justify-between my-20">
       <div className="order-2 md:order-1 text-center md:text-left mt-10 md:mt-0">
@@ -13,19 +18,40 @@ export default function PortfolioHero() {
         </button> */}
 
         <div className="flex justify-center md:justify-start">
-          <button className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold">
-            Linkedin
-          </button>
-          <button className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold">
-            Github
-          </button>
-          <button className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold">
-            Email
-          </button>
-          <button className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold">
-            Facebook
-          </button>
+          <Link
+            as="https://www.linkedin.com/in/abdullah97/"
+            href="https://www.linkedin.com/in/abdullah97/"
+          >
+            <a
+              className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+          </Link>
+          <Link as="https://github.com/xayden" href="https://github.com/xayden">
+            <a
+              className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold"
+              target="_blank"
+            >
+              Github
+            </a>
+          </Link>
+          <Link
+            as="mailto:abdullah.osama.abdelmajeed@gmail.com"
+            href="mailto:abdullah.osama.abdelmajeed@gmail.com"
+          >
+            <a
+              className="py-2 px-3 rounded-lg mr-4 border border-black hover:bg-black hover:text-white font-semibold"
+              onClick={() => setShowEmail(true)}
+            >
+              Email
+            </a>
+          </Link>
         </div>
+        {showEmail ? (
+          <p className="my-4">abdullah.osama.abdelmajeed@gmail.com</p>
+        ) : null}
       </div>
 
       {/* <div className="order-1 md:order-2 ml-5">
