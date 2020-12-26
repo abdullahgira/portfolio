6c324bacd15e1a80dbae59edce71ca7d38f822df
Xayden Rosario <xaydenrosario@gmail.com>
2020-12-26 19:04:05 +0200

    Upgrade the skills section to the next level

--- components/skills-section.js
+++ components/skills-section.js
@@ -14,65 +14,46 @@ export default function SkillsSection() {
         <h2 className="text-3xl text-center font-bold">My Skills</h2>
       </div>
 
-      <div className="flex flex-wrap justify-around relative z-0">
-        <Skill>
-          <img className="w-32" src="portfolio/react.svg" alt="react logo" />
-          <h4 className="mb-4">React</h4>
+      <div class="flex flex-wrap gap-8 justify-around">
+        <Skill logo="portfolio/react.svg">
+          <h4 className="font-bold text-2xl tracking-tighter mb-2">React</h4>
           <p>
             I use react.js library for building high perfromance user interface.
           </p>
         </Skill>
 
-        <Skill>
-          <img className="w-32" src="portfolio/nodejs.svg" alt="nodejs logo" />
-          <h4 className="mb-4">Node.js</h4>
+        <Skill logo="portfolio/nodejs.svg">
+          <h4 className="font-bold text-2xl tracking-tighter mb-2">Node.js</h4>
           <p>I use node.js for the backend for all of my projects.</p>
         </Skill>
 
-        <Skill>
-          <img
-            className="w-32"
-            src="portfolio/mongodb.svg"
-            alt="mongodb logo"
-          />
-          <h4 className="mb-4">MongoDB</h4>
-          <p>Is my go to database without a second thought.</p>
+        <Skill logo="portfolio/mongodb.svg">
+          <h4 className="font-bold text-2xl tracking-tighter mb-2">MongoDB</h4>
+          <p>
+            Is my go to database without a second thought because of it's high
+            perfromance
+          </p>
         </Skill>
 
-        <img
-          src="portfolio/circles-colored.svg"
-          className="w-20 absolute -top-14 right-4 md:-right-8"
-        />
-      </div>
-
-      <div className="flex flex-wrap justify-center gap-8">
-        <Skill>
-          <img className="w-32" src="portfolio/redux.svg" alt="redux logo" />
-          <h4 className="mb-4">Redux</h4>
+        <Skill logo="portfolio/redux.svg">
+          <h4 className="font-bold text-2xl tracking-tighter mb-2">Redux</h4>
           <p>
             I only use it when global state in react is a must to acheive the
             goals of the app.
           </p>
-        </Skill>
-
-        <Skill>
-          <img
-            className="h-32"
-            src="portfolio/html-css-js.svg"
-            alt="html, css and javascript logos"
-          />
-          <h4 className="mb-4">HTML, CSS & Javascript</h4>
-          <p>The basic building blocks for my frontend.</p>
         </Skill>
       </div>
     </div>
   )
 }
 
-function Skill({children}) {
+function Skill({logo, children}) {
   return (
-    <div className="blur-bg rounded-lg border border-indigo-800 mb-14 px-5 py-12 w-80 flex flex-col items-center text-center">
-      {children}
+    <div className="bg-white rounded-xl flex p-5 shadow-xl max-w-lg">
+      <div className="p-6 rounded-xl bg-purple-900 shadow-xl inline-block mr-5 flex-none">
+        <img className="w-20" src={logo} alt="react logo" />
+      </div>
+      <div className="mt-2 inline-block">{children}</div>
     </div>
   )
 }
