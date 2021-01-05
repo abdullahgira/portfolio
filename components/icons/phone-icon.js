4f7e1bcee4eec3099b2314b8c4fbe2697af90917
Xayden Rosario <xaydenrosario@gmail.com>
2021-01-05 09:12:52 +0200

    Add social icons

---
+++ components/icons/phone-icon.js
@@ -0,0 +1,17 @@
+export default function PhoneIcon({size, className}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path
+        fill="#4965CD"
+        d="M12.706 13.425l1.4-1.4a1.369 1.369 0 011.463-.3l1.706.681a1.36 1.36 0 01.85 1.25v3.125a1.356 1.356 0 01-1.425 1.344C4.744 17.381 2.331 7.256 1.875 3.381a1.356 1.356 0 011.356-1.506H6.25a1.35 1.35 0 011.25.85l.681 1.706a1.35 1.35 0 01-.293 1.463l-1.4 1.4s.806 5.456 6.218 6.131z"
+      ></path>
+    </svg>
+  )
+}
