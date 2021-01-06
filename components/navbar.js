d09b15fa2df884fb2cca1f26867518262d186064
Xayden Rosario <xaydenrosario@gmail.com>
2021-01-06 11:04:13 +0200

    Update the logo dot

--- components/navbar.js
+++ components/navbar.js
@@ -10,13 +10,13 @@ export default function Navbar() {
     <nav className="mb-10 border-t-4 border-blue-700">
       <div className="max-w-5xl mx-auto px-5 md:px-8">
         <div className="flex justify-between items-center">
-          <div className="py-2 mr-auto">
+          <div className="py-2 mr-auto -mt-1">
             <Link as="/" href="/">
               <a>
                 <span className="text-lg font-bold tracking-tight">
                   Abdullah Gira
                 </span>
-                <span className="text-3xl text-blue-700">.</span>
+                <span className="text-4xl text-blue-700">.</span>
               </a>
             </Link>
           </div>
