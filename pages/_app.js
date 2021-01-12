b37ec785e7f97b615d771942acb2848133fde956
Abdullah Osama <xaydenrosario@gmail.com>
2021-01-12 16:38:52 +0200

    Add head meta tags for seo

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,15 @@ import React from "react"
 import "tailwindcss/tailwind.css"
 import "styles/globals.css"
 import Navbar from "components/navbar"
+import Head from "next/head"
 
 function MyApp({Component, pageProps}) {
   return (
     <React.Fragment>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </React.Fragment>
--- pages/projects/issue-tracker.js
+++ pages/projects/issue-tracker.js
@@ -1,140 +1,152 @@
+import Head from "next/head"
 import Plyr from "plyr-react"
 import "plyr-react/dist/plyr.css"
 
 export default function IssueTracker() {
   return (
-    <div className="max-w-3xl m-auto my-4 sm:my-16 px-8">
-      <h1 className="text-4xl font-bold text-center my-20">
-        iTracker - Issue Tracking System
-      </h1>
-      <Plyr
-        source={{
-          title: "itracker demo",
-          type: "video",
-          sources: [
+    <div>
+      <Head>
+        <title>iTracker - Issue Tracking System</title>
+        <meta
+          content="A project planning system that helps manage teams and keep them on
+          track"
+          name="description"
+        />
+      </Head>
+      <div className="max-w-3xl m-auto my-4 sm:my-16 px-8">
+        <h1 className="text-4xl font-bold text-center my-20">
+          iTracker - Issue Tracking System
+        </h1>
+        <Plyr
+          source={{
+            title: "itracker demo",
+            type: "video",
+            sources: [
+              {
+                src: "/projects/itracker/itracker-demo.mp4",
+                provider: "html5",
+                type: "video/mp4",
+                size: 720,
+              },
+            ],
+            // poster: "/projects/itracker-thumbnail.png",
+          }}
+          options={
             {
-              src: "/projects/itracker/itracker-demo.mp4",
-              provider: "html5",
-              type: "video/mp4",
-              size: 720,
-            },
-          ],
-          // poster: "/projects/itracker-thumbnail.png",
-        }}
-        options={
-          {
-            // autoplay: true,
+              // autoplay: true,
+            }
           }
-        }
-      />
+        />
 
-      <h2 className="text-2xl font-bold text-center my-20">Features</h2>
+        <h2 className="text-2xl font-bold text-center my-20">Features</h2>
 
-      <ProjectSection>
-        <ProjectSectionImg>
-          <img
-            className="shadow-sm rounded-lg"
-            src="/projects/itracker/your-work-glance.png"
-            alt="your work page with some issues present"
-          />
-        </ProjectSectionImg>
-        <ProjectSectionDescription>
-          <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
-            Your work at a glance
-          </h3>
+        <ProjectSection>
+          <ProjectSectionImg>
+            <img
+              className="shadow-sm rounded-lg"
+              src="/projects/itracker/your-work-glance.png"
+              alt="your work page with some issues present"
+            />
+          </ProjectSectionImg>
+          <ProjectSectionDescription>
+            <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
+              Your work at a glance
+            </h3>
 
-          <p className="text-gray-500">
-            See the issues assigned to you with their priorities from all the
-            projects, so you could effortlessly decide which issue to start
-            working on.
-          </p>
-        </ProjectSectionDescription>
-      </ProjectSection>
+            <p className="text-gray-500">
+              See the issues assigned to you with their priorities from all the
+              projects, so you could effortlessly decide which issue to start
+              working on.
+            </p>
+          </ProjectSectionDescription>
+        </ProjectSection>
 
-      <ProjectSection>
-        <ProjectSectionImg isOdd>
-          <img
-            className="shadow-sm rounded-lg"
-            src="/projects/itracker/project-dashboard.png"
-            alt="project general information and progress"
-          />
-        </ProjectSectionImg>
-        <ProjectSectionDescription isOdd>
-          <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
-            Overall progress of the project and the team members
-          </h3>
+        <ProjectSection>
+          <ProjectSectionImg isOdd>
+            <img
+              className="shadow-sm rounded-lg"
+              src="/projects/itracker/project-dashboard.png"
+              alt="project general information and progress"
+            />
+          </ProjectSectionImg>
+          <ProjectSectionDescription isOdd>
+            <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
+              Overall progress of the project and the team members
+            </h3>
 
-          <p className="text-gray-500">
-            See how many projects are open, in-progress, or closed, of what
-            priority are they, and of what type. You can also see the overall
-            progress of your team members based on the issues assigned to them.
-          </p>
-        </ProjectSectionDescription>
-      </ProjectSection>
+            <p className="text-gray-500">
+              See how many projects are open, in-progress, or closed, of what
+              priority are they, and of what type. You can also see the overall
+              progress of your team members based on the issues assigned to
+              them.
+            </p>
+          </ProjectSectionDescription>
+        </ProjectSection>
 
-      <ProjectSection>
-        <ProjectSectionImg>
-          <img
-            className="shadow-sm rounded-lg"
-            src="/projects/itracker/comments-and-history.png"
-            alt="your work page with some issues present"
-          />
-        </ProjectSectionImg>
-        <ProjectSectionDescription>
-          <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
-            Comments and history of changes for each issue
-          </h3>
+        <ProjectSection>
+          <ProjectSectionImg>
+            <img
+              className="shadow-sm rounded-lg"
+              src="/projects/itracker/comments-and-history.png"
+              alt="your work page with some issues present"
+            />
+          </ProjectSectionImg>
+          <ProjectSectionDescription>
+            <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
+              Comments and history of changes for each issue
+            </h3>
 
-          <p className="text-gray-500">
-            Keep track of issue subtasks in the form of comments, and see all
-            the changes that happened to the issue.
-          </p>
-        </ProjectSectionDescription>
-      </ProjectSection>
+            <p className="text-gray-500">
+              Keep track of issue subtasks in the form of comments, and see all
+              the changes that happened to the issue.
+            </p>
+          </ProjectSectionDescription>
+        </ProjectSection>
 
-      <ProjectSection>
-        <ProjectSectionImg isOdd>
-          <img
-            className="shadow-sm rounded-lg"
-            src="/projects/itracker/role-management.png"
-            alt="project general information and progress"
-          />
-        </ProjectSectionImg>
-        <ProjectSectionDescription isOdd>
-          <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
-            Role management system
-          </h3>
+        <ProjectSection>
+          <ProjectSectionImg isOdd>
+            <img
+              className="shadow-sm rounded-lg"
+              src="/projects/itracker/role-management.png"
+              alt="project general information and progress"
+            />
+          </ProjectSectionImg>
+          <ProjectSectionDescription isOdd>
+            <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
+              Role management system
+            </h3>
 
-          <p className="text-gray-500">
-            Manage the roles of each one of your team members. With 3 different
-            roles, leader, submitter, and developer. Leader role can only be
-            assigned from the Project Settings page.
-          </p>
-        </ProjectSectionDescription>
-      </ProjectSection>
+            <p className="text-gray-500">
+              Manage the roles of each one of your team members. With 3
+              different roles, leader, submitter, and developer. Leader role can
+              only be assigned from the Project Settings page.
+            </p>
+          </ProjectSectionDescription>
+        </ProjectSection>
 
-      <h2 className="text-2xl font-bold text-center my-20">
-        Obstacles I overcame
-      </h2>
+        <h2 className="text-2xl font-bold text-center my-20">
+          Obstacles I overcame
+        </h2>
 
-      <div>
-        <div className="p-5 bg-indigo-100 rounded-lg shadow-md mb-5">
-          <h3 className="text-xl">Auto refreshing the access token</h3>
-        </div>
+        <div>
+          <div className="p-5 bg-indigo-100 rounded-lg shadow-md mb-5">
+            <h3 className="text-xl">Auto refreshing the access token</h3>
+          </div>
 
-        <div className="p-5 bg-yellow-100 rounded-lg shadow-md mb-5">
-          <h3 className="text-xl">Building the backend for scalability</h3>
-        </div>
+          <div className="p-5 bg-yellow-100 rounded-lg shadow-md mb-5">
+            <h3 className="text-xl">Building the backend for scalability</h3>
+          </div>
 
-        <div className="p-5 bg-red-100 rounded-lg shadow-md mb-5">
-          <h3 className="text-xl">Making of the editable header</h3>
-        </div>
+          <div className="p-5 bg-red-100 rounded-lg shadow-md mb-5">
+            <h3 className="text-xl">Making of the editable header</h3>
+          </div>
 
-        <div className="p-5 bg-green-100 rounded-lg shadow-md">
-          <h3 className="text-xl">
-            Architecting the frontend for minimum re-rendering of the
-            components.
-          </h3>
+          <div className="p-5 bg-green-100 rounded-lg shadow-md">
+            <h3 className="text-xl">
+              Architecting the frontend for minimum re-rendering of the
+              components.
+            </h3>
+          </div>
         </div>
       </div>
     </div>
