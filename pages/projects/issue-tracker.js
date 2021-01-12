7eb24942b034c5829892b5c6aed4ea6bfa656b41
Abdullah Osama <xaydenrosario@gmail.com>
2021-01-12 13:01:57 +0200

    Added first itracker feature

--- pages/projects/issue-tracker.js
+++ pages/projects/issue-tracker.js
@@ -3,15 +3,17 @@ import "plyr-react/dist/plyr.css"
 
 export default function IssueTracker() {
   return (
-    <div className="max-w-2xl m-auto my-4 sm:my-16">
-      <h1 className="text-4xl font-bold text-center mb-20">Issue Tracker</h1>
+    <div className="max-w-4xl m-auto my-4 sm:my-16">
+      <h1 className="text-4xl font-bold text-center my-20">
+        iTracker - Issue Tracking System
+      </h1>
       <Plyr
         source={{
           title: "itracker demo",
           type: "video",
           sources: [
             {
-              src: "/projects/itracker-demo.mp4",
+              src: "/projects/itracker/itracker-demo.mp4",
               provider: "html5",
               type: "video/mp4",
               size: 720,
@@ -19,10 +21,51 @@ export default function IssueTracker() {
           ],
           // poster: "/projects/itracker-thumbnail.png",
         }}
-        options={{
-          autoplay: true,
-        }}
+        options={
+          {
+            // autoplay: true,
+          }
+        }
       />
+
+      <h2 className="text-2xl font-bold text-center my-20">Features</h2>
+
+      <FeatureSection>
+        <FeatureImg>
+          <img
+            className="shadow-sm rounded-lg"
+            src="/projects/itracker/your-work-glance.png"
+            alt="your work page with some issues present"
+          />
+        </FeatureImg>
+        <FeatureDescription>
+          <h3 className="text-xl font-semibold mb-10 lg:w-96 mt-10 lg:mt-0">
+            Your work at a glance
+          </h3>
+
+          <p className="text-gray-500">
+            See the issues assigned to you with their priorities from all the
+            projects, so you could effortlessly decide which issue to start
+            working on.
+          </p>
+        </FeatureDescription>
+      </FeatureSection>
     </div>
   )
 }
+
+const FeatureSection = ({children}) => (
+  <div className="flex flex-col content-center items-center lg:items-start lg:flex-row mb-32 lg:mb-48">
+    {children}
+  </div>
+)
+
+const FeatureImg = ({children}) => (
+  <div className="max-w-xl mx-auto lg:max-w-none lg:w-3/5 lg:mr-20">
+    {children}
+  </div>
+)
+
+const FeatureDescription = ({children}) => (
+  <div className="max-w-xs sm:max-w-sm">{children}</div>
+)
