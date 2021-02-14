a5b22db2cb7192f2348a60d3179528e74e1ec4cc
Abdullah Gira <xaydenrosario@gmail.com>
2021-02-14 21:08:08 +0200

    Update the thickness of the border bottom of active navbar link

--- components/active-link.js
+++ components/active-link.js
@@ -10,7 +10,7 @@ export default function ActiveLink({
   const router = useRouter()
   const styleClassName = `py-2 hover:text-gray-700 ${className} ${
     router?.pathname.match(activeRegExp)
-      ? "text-gray-700 border-b-2 border-blue-700 font-bold px-2"
+      ? "text-gray-700 border-b-4 border-blue-700 font-bold px-2"
       : "text-gray-500"
   }`
 
