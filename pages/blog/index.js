776cae90c603bc4c5336c8bd12552ba810d3dbf5
Abdullah Gira <abdullaho.gira@gmail.com>
2022-11-20 04:18:11 +0200

    Add revue subscription form

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -45,6 +45,70 @@ export default function BlogPage({allPosts}) {
           className="w-20 bg-blue-700 rounded-full"
           alt="my profile picture"
         />
+
+        <div id="revue-embed">
+          <form
+            action="https://www.getrevue.co/profile/abdullahgira/add_subscriber"
+            method="post"
+            id="revue-form"
+            name="revue-form"
+            target="_blank"
+          >
+            <div class="revue-form-group">
+              <label for="member_email">Email address</label>
+              <input
+                class="revue-form-field"
+                placeholder="Your email address..."
+                type="email"
+                name="member[email]"
+                id="member_email"
+              />
+            </div>
+            <div class="revue-form-group">
+              <label for="member_first_name">
+                First name <span class="optional">(Optional)</span>
+              </label>
+              <input
+                class="revue-form-field"
+                placeholder="First name... (Optional)"
+                type="text"
+                name="member[first_name]"
+                id="member_first_name"
+              />
+            </div>
+            <div class="revue-form-group">
+              <label for="member_last_name">
+                Last name <span class="optional">(Optional)</span>
+              </label>
+              <input
+                class="revue-form-field"
+                placeholder="Last name... (Optional)"
+                type="text"
+                name="member[last_name]"
+                id="member_last_name"
+              />
+            </div>
+            <div class="revue-form-actions">
+              <input
+                type="submit"
+                value="Subscribe"
+                name="member[subscribe]"
+                id="member_submit"
+              />
+            </div>
+            <div class="revue-form-footer">
+              By subscribing, you agree with Revue’s{" "}
+              <a target="_blank" href="https://www.getrevue.co/terms">
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a target="_blank" href="https://www.getrevue.co/privacy">
+                Privacy Policy
+              </a>
+              .
+            </div>
+          </form>
+        </div>
       </section>
       <section className="mt-20">
         {allPosts.length > 0
