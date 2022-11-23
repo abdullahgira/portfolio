8ae43bb093af1826261bba4a711d2adc5d459964
Abdullah Gira <abdullaho.gira@gmail.com>
2022-11-23 23:22:49 +0200

    Use absolute path to get avatar image

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ const Avatar = () => {
   return (
     <div className="w-24 h-24 bg-blue-900 bg-opacity-40 object-cover rounded-full overflow-hidden">
       <img
-        src="portfolio/me.png"
+        src="/portfolio/me.png"
         alt="my profile picture"
         className="w-32 h-32 object-contain"
       />
