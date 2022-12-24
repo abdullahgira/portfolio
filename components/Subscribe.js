51716304c9f344d9f87383ccc4e132d2b8b869a5
Abdullah Gira <abdullaho.gira@gmail.com>
2022-12-24 06:54:26 +0200

    Add a comment for powered by revue link

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import React from "react"
 
 import Button from "./Button"
@@ -13,7 +14,7 @@ const Subscribe = () => {
         name="revue-form"
         target="_blank"
       >
-        <div className="flex items-center gap-4 mb-2 flex-wrap">
+        <div className="flex items-start gap-4 mb-2 flex-wrap">
           <div>
             <input
               className="bg-white shadow-md rounded-full px-5 py-3 w-60 md:w-auto"
@@ -22,10 +23,20 @@ const Subscribe = () => {
               name="member[email]"
             />
           </div>
-          <div>
+          <div className="flex flex-col items-end">
             <Button filled type="submit">
               Subscribe
             </Button>
+            {/* <small className="text-gray-500 mt-1">
+              <i>
+                Powered by{" "}
+                <Link href="https://www.getrevue.co/">
+                  <a className="underline" target="_blank">
+                    Revue
+                  </a>
+                </Link>
+              </i>
+            </small> */}
           </div>
         </div>
       </form>
