611e1d262b32d3fdfb535c9d8784c23d196d55b6
Abdullah Gira <abdullaho.gira@gmail.com>
2023-01-03 14:37:01 +0200

    Add preview draft

--- frontend/components/DateFormatter.js
+++ frontend/components/DateFormatter.js
@@ -1,6 +1,8 @@
 import {parseISO, format} from "date-fns"
 
 export default function DateFormatter({dateString}) {
+  if (!dateString) return "Publish date"
+
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, "d LLLL yyyy")}</time>
 }
