0519b6eea0f2e57a835040470faef8d14f45605b
Abdullah Gira <abdullaho.gira@gmail.com>
2023-01-13 21:18:17 +0200

    Fix about links

--- frontend/components/IconWrapper.js
+++ frontend/components/IconWrapper.js
@@ -1,7 +1,16 @@
 import React from "react"
+import Link from "next/link"
 
-const IconWrapper = ({className, children}) => {
-  return <div className={`w-5 ${className}`}>{children}</div>
+const IconWrapper = ({className, link = "", children}) => {
+  return (
+    <div className={`w-5 ${className}`}>
+      <Link href={link}>
+        <a target="_blank" rel="noopener">
+          {children}
+        </a>
+      </Link>
+    </div>
+  )
 }
 
 export default IconWrapper
